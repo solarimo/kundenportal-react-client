c15c8abd9c1b06b70c309273074575c7a89269ba
martingrzzler <martin.gressler@code.berlin>
2020-08-27 08:46:43 +0200

    refactored Navbar into Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Header } from './header/Header';
 import { Footer } from './footer/Footer';
-import { Navbar } from './navbar/Navbar'
+import { Navbar } from './header/navbar/Navbar'
 
 
 export class App extends React.Component {
@@ -10,7 +10,6 @@ export class App extends React.Component {
         return (
             <div className="main">
                 <Header />
-                <Navbar />
                 <div className="content">Main Content</div>
                 <Footer />
             </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent } from 'react';
-import { Navbar } from '../navbar/Navbar';
+import { Navbar } from './navbar/Navbar';
+import './Header.css';
 
 export const Header: FunctionComponent = () => {
     return (
         <div className="header">
-            <img height="60" src={'./Logo_SolarME-300x71.png'} alt="Solarme Logo"/>
-            <h1>Mein Kundenportal</h1>
+            <div className="headline">
+                <img height="60" src={'./Logo_SolarME-300x71.png'} alt="Solarme Logo"/>
+                <h1>Mein Kundenportal</h1>
+            </div>
+            <div>
+                <Navbar />
+            </div>
         </div>
     );
 }
