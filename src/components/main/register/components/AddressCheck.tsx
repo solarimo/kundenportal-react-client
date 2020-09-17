d565804cc7caae2f56dc33f3cc4d27dffadcc0a6
martingrzzler <martin.gressler@code.berlin>
2020-09-17 20:23:47 +0200

    minor layout adjustment in AddressCheck

--- src/components/main/register/components/AddressCheck.tsx
+++ src/components/main/register/components/AddressCheck.tsx
@@ -8,28 +8,29 @@ export class AddressCheck extends React.Component {
   render() {
     return (
       <div className="address-component">
-          <h3>Bitte nennen Sie uns Ihre Adresse. Wir prüfen für Sie, ob der Solarstrom vom Dach in Ihrer Wohnung verfügbar ist.</h3>
-          <br/>
-          <form>
+        <h2>Bitte nennen Sie uns Ihre Adresse. Wir prüfen für Sie, ob der Solarstrom vom Dach in Ihrer Wohnung verfügbar ist.</h2>
+        <form>
+          <div id="address-container">
             <FormField
               id="address-input"
               labelName="Suchen sie hier nach Ihrer Adresse:"
             />
-            <div id="address-container">
-              <div className="flex-container">
-                <FormField id="strasse" labelName="Straße" width="75%" />
-                <FormField id="hausnummer" labelName="Hausnummer" width="25%" />
-              </div>
-              <div className="flex-container">
-                <FormField id="postleitzahl" labelName="Postleitzahl" width="50%" />
-                <FormField id="stadt" labelName="Stadt" width="50%" />
-              </div>
+            <br/>
+            <div className="flex-container">
+              <FormField id="strasse" labelName="Straße" width="75%" />
+              <FormField id="hausnummer" labelName="Hausnummer" width="25%" />
+            </div>
+            <div className="flex-container">
+              <FormField id="postleitzahl" labelName="Postleitzahl" width="50%" />
+              <FormField id="stadt" labelName="Stadt" width="50%" />
             </div>
             <div className="flex-btn">
-              <PrimaryButton text="Zu Ihrem Tarif"/>
+              <PrimaryButton text="Zu Ihrem Tarif" />
             </div>
-          </form>
-        </div>
+          </div>
+
+        </form>
+      </div>
     );
   }
 }
