edcae2af81c8884323686204aa8125d080a1d504
martingrzzler <martin.gressler@code.berlin>
2020-09-17 11:21:13 +0200

    added redux with dummy reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import { App } from './components/App';
 import './index.css';
+import { reducers } from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
 
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
