8a1d446980f7f48319c34ac76a588ebfffddcdc6
martingrzzler <martin.gressler@code.berlin>
2020-09-25 15:51:29 +0200

    calculator upper part working with component did update

--- src/components/main/register/components/calculator/Calculator.tsx
+++ src/components/main/register/components/calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState, createRef } from 'react';
+import React, { FormEvent, useState, createRef, useEffect } from 'react';
 import { mustBeNumber } from '../../../../util/ValidationRules';
 
 import './Calculator.css';
@@ -12,17 +12,27 @@ const labels: { [key: string]: { ref: React.RefObject<HTMLLabelElement>, value: 
   l5: { ref: createRef<HTMLLabelElement>(), value: 5 },
 }
 
-const input = createRef<HTMLInputElement>();
+interface CalculatorState {
+  stromverbrauch: string;
+  errorMessage: string;
+}
+
+const Error = ({ message }: { message: string }) => {
+  return <span style={{ color: 'red', fontSize: '10px' }} >{message}</span>;
+}
+
+class Calculator extends React.Component<{}, CalculatorState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      stromverbrauch: '',
+      errorMessage: ''
+    }
+  }
 
 
-
-const Calculator = () => {
-
-
-  const [stromverbrauch, setStromverbrauch] = useState('');
-  const [error, setError] = useState('');
-
-  const setStromverbrauchByPersonen = (e: FormEvent<HTMLInputElement>) => {
+  setStromverbrauchByPersonen = (e: FormEvent<HTMLInputElement>) => {
     const numOfId = parseInt(e.currentTarget.id[1]);
     for (const label in labels) {
       const current = labels[label].ref.current;
@@ -34,25 +44,27 @@ const Calculator = () => {
       }
 
     }
-    setStromverbrauch(e.currentTarget.value);
-
+    this.setState({ stromverbrauch: e.currentTarget.value });
+    
   }
 
-  const onInputChange = (e: FormEvent<HTMLInputElement>) => {
+  onInputChange = (e: FormEvent<HTMLInputElement>) => {
 
 
     const error = mustBeNumber(parseInt(e.currentTarget.value));
-    if (error) setError(error); else setError('');
-    setStromverbrauch(e.currentTarget.value);
-
-
-    console.log(stromverbrauch);
-
-
+    this.setState({ errorMessage: error ? error : '' });
+    this.setState({ stromverbrauch: e.currentTarget.value });
   }
 
+  componentDidUpdate() {
+    console.log(this.state.stromverbrauch);
+    console.log(this.state.errorMessage);
+    
+  }
 
-  return (
+  render() { 
+    
+    return (
     <div className="calc">
       <form>
         <div className="calc-top">
@@ -62,36 +74,37 @@ const Calculator = () => {
               <label ref={labels.l1.ref} htmlFor="p1">
                 <img src={'/icon_person.png'} alt="person-img" className="person-img" />
               </label>
-              <input onChange={setStromverbrauchByPersonen} type="radio" id="p1" value={1500} name="person-amount" />
+              <input onClick={this.setStromverbrauchByPersonen} type="radio" id="p1" value={1500} name="person-amount" />
               <label ref={labels.l2.ref} htmlFor="p2">
                 <img src={'/icon_person.png'} alt="person-img" className="person-img" />
               </label>
-              <input onChange={setStromverbrauchByPersonen} type="radio" id="p2" value={2500} name="person-amount" />
+              <input onClick={this.setStromverbrauchByPersonen} type="radio" id="p2" value={2500} name="person-amount" />
               <label ref={labels.l3.ref} htmlFor="p3">
                 <img src={'/icon_person.png'} alt="person-img" className="person-img" />
               </label>
-              <input onChange={setStromverbrauchByPersonen} type="radio" id="p3" value={3500} name="person-amount" />
+              <input onClick={this.setStromverbrauchByPersonen} type="radio" id="p3" value={3500} name="person-amount" />
               <label ref={labels.l4.ref} htmlFor="p4">
                 <img src={'/icon_person.png'} alt="person-img" className="person-img" />
               </label>
-              <input onChange={setStromverbrauchByPersonen} type="radio" id="p4" value={4250} name="person-amount" />
+              <input onClick={this.setStromverbrauchByPersonen} type="radio" id="p4" value={4250} name="person-amount" />
               <label ref={labels.l5.ref} htmlFor="p5">
                 <img src={'/icon_person.png'} alt="person-img" className="person-img" />
               </label>
-              <input ref={input} onChange={setStromverbrauchByPersonen} type="radio" id="p5" value={5000} name="person-amount" />
+              <input onClick={this.setStromverbrauchByPersonen} type="radio" id="p5" value={5000} name="person-amount" />
             </div>
           </div>
           <div>
             <p>oder Stromverbrauch (kWh/Jahr)</p>
-            <input ref={input} value={stromverbrauch} onChange={onInputChange} />
+            <input className={ this.state.errorMessage ? 'field-error' : '' } value={this.state.stromverbrauch} onChange={this.onInputChange} />
+            { this.state.errorMessage && <Error message={this.state.errorMessage} /> }
           </div>
         </div>
-
         <div className="calc-bottom">
         </div>
       </form>
     </div>
   )
+  }
 }
 
 export default Calculator;
