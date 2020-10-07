187a4dd7f6853356734c32959e45cec2b625615b
martingrzzler <martin.gressler@code.berlin>
2020-10-07 19:49:50 +0200

    modified depl

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const BACKEND_BASE_URL = process.env.BACKEND_BASE_URL || 'http://localhost:8080';
+const BACKEND_BASE_URL = 'http://localhost:8080/api/v1';
 
 export default Axios.create({
   baseURL: BACKEND_BASE_URL
