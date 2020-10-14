593dfde8b955a987eaeddfa5fca5cbf9269dcf0b
martingrzzler <martin.gressler@code.berlin>
2020-10-14 16:03:20 +0200

    persoenlcihe Daten addon

--- src/components/main/register/components/vertragsdaten/PersoenlicheDaten.tsx
+++ src/components/main/register/components/vertragsdaten/PersoenlicheDaten.tsx
@@ -1,10 +1,37 @@
 import React, { FunctionComponent } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { renderInput, renderSelect, Value } from '../../../../util/renderField';
 
+const anreden: Value[] = [
+  { value: 'HERR', displayed: 'Herr' },
+  { value: 'FRAU', displayed: 'Frau' },
+  { value: 'DIVERS', displayed: 'Divers' }
+]
 
-export const PersoenlicheDaten: FunctionComponent = () => {
+const titles: Value[] = [
+  { value: 'DR', displayed: 'Dr.' },
+  { value: 'PROF', displayed: 'Prof.' },
+  { value: 'PROF_DR', displayed: 'Prof. Dr.' }
+]
+
+const _PersoenlicheDaten: FunctionComponent = () => {
   return (
     <div>
-      S
+      <h2>PERSÖNLICHE ANGABEN</h2>
+      <p>Damit wir Ihren Vertrag aufsetzen können, benötigen wir einige Informationen von Ihnen. Im darauffolgenden Schritt können Sie Ihre Angaben noch einmal überprüfen.</p>
+      <form action="">
+        <Field name="anrede" label="Anrede" component={renderSelect} values={anreden} />
+        <Field name="titel" label="Titel" component={renderSelect} values={titles} />
+        <Field name="vorname" label="Vorname" component={renderInput} />
+        <Field name="nachname" label="Nachname" component={renderInput} />  
+        <Field name="geburtsdatum" label="Geburtsdatum" component={renderInput} />
+      </form>
     </div>
   );
 }
+
+export const PersoenlicheDaten = reduxForm({
+  form: 'vertragsdaten',
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true
+})(_PersoenlicheDaten);
--- src/components/util/renderField.tsx
+++ src/components/util/renderField.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-export function renderInput({label, input, meta}: any) {
+export interface Value {
+  value: string;
+  displayed: string;
+}
+
+export function renderInput({ label, input, meta }: any) {
   return (
     <div>
       <label>{label}</label>
-      <input className={(meta.touched && meta.error) ? 'field-error' : '' } {...input} type="text" />
-  {(meta.touched && meta.error) &&  <span style={{ color: 'red', fontSize: '10px' }} >{meta.error}</span> }
+      <input className={(meta.touched && meta.error) ? 'field-error' : ''} {...input} type="text" />
+      {(meta.touched && meta.error) && <span style={{ color: 'red', fontSize: '10px' }} >{meta.error}</span>}
     </div>
   );
+}
+
+export function renderSelect({ label, input, meta, values }: any) {
+  return (
+    <div>
+      <label>{label}</label>
+      <select>
+        <option></option>
+        {renderOptions(values as Value[])}
+      </select>
+    </div>
+  );
+}
+
+
+function renderOptions(values: Value[]) {
+  return values.map(value => {
+    return <option key={value.value} value={value.value}>{value.displayed}</option>
+  });
 }
